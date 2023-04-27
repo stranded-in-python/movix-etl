ad7a7eda88ca8399d1fff1a16bf944bcfa06c655
sbkubric <sbkubric@gmail.com>
2023-04-27 14:18:39 +0300

    fix: empty person fields

--- src/postgres_to_es/models.py
+++ src/postgres_to_es/models.py
@@ -1,22 +1,25 @@
 from datetime import datetime
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class Entry(BaseModel):
-    modified: datetime
     id: UUID
+    modified: datetime
+    _transform_elastic = validator("id", allow_reuse=True)(lambda x: str(x) if x else x)
 
 
 class CrewMember(BaseModel):
     id: UUID
     full_name: str
+    _transform_elastic = validator("id", allow_reuse=True)(lambda x: str(x) if x else x)
 
 
 class Genre(BaseModel):
     id: UUID
     name: str
+    _transform_elastic = validator("id", allow_reuse=True)(lambda x: str(x) if x else x)
 
 
 class FilmWork(BaseModel):
@@ -24,8 +27,9 @@ class FilmWork(BaseModel):
     title: str
     description: str | None
     rating: float
-    genre: list[str]
-    actors: list[CrewMember]
-    directors: list[CrewMember]
-    writers: list[CrewMember]
-    genres: list[Genre]
+    genre: tuple[str, ...]
+    actors: tuple[CrewMember, ...]
+    directors: tuple[CrewMember, ...]
+    writers: tuple[CrewMember, ...]
+    genres: tuple[Genre, ...]
+    _transform_elastic = validator("id", allow_reuse=True)(lambda x: str(x) if x else x)
