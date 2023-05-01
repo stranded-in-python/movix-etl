0d329809a747dd89de3f59d906f834bff63dc859
sbkubric <sbkubric@gmail.com>
2023-05-01 20:38:31 +0300

    fix: flake8

--- src/postgres_to_es/mergers.py
+++ src/postgres_to_es/mergers.py
@@ -109,68 +109,6 @@ class FilmWorkPostgresMerger(PostgresMerger):
         return entries
 
 
-class PersonPostgresMerger(PostgresMerger):
-    sql = (
-        "SELECT fw.id, fw.title, fw.description, fw.rating,"
-        "array_agg(DISTINCT g.name) as genre, "
-        """COALESCE (
-               json_agg(
-                   DISTINCT jsonb_build_object(
-                       'id', p.id,
-                       'full_name', p.full_name
-                   )
-               ) FILTER (WHERE p.id is not null and pfw.role = 'actor'),
-               '[]'
-           ) as actors, """
-        """COALESCE (
-               json_agg(
-                   DISTINCT jsonb_build_object(
-                       'id', p.id,
-                       'full_name', p.full_name
-                   )
-               ) FILTER (WHERE p.id is not null and pfw.role = 'director'),
-               '[]'
-           ) as directors, """
-        """COALESCE (
-               json_agg(
-                   DISTINCT jsonb_build_object(
-                       'id', p.id,
-                       'full_name', p.full_name
-                   )
-               ) FILTER (WHERE p.id is not null and pfw.role = 'writer'),
-               '[]'
-           ) as writers ,"""
-        """COALESCE (
-                json_agg(
-                    DISTINCT jsonb_build_object('id', g.id, 'name', g.name)
-                ) FILTER (WHERE g.id is not null and gfw.genre_id = g.id),
-                '[]'
-            ) as genres """
-        "FROM content.film_work fw "
-        "LEFT JOIN content.person_film_work pfw ON pfw.film_work_id = fw.id "
-        "LEFT JOIN content.person p ON p.id = pfw.person_id "
-        "LEFT JOIN content.genre_film_work gfw ON gfw.film_work_id = fw.id "
-        "LEFT JOIN content.genre g ON g.id = gfw.genre_id "
-        "WHERE fw.id IN %s GROUP BY fw.id ORDER BY fw.modified ASC ;"
-    )
-
-    def transform_to_entries(
-        self, rows: Iterable[DictRow]
-    ) -> Iterable[FilmWorkDocument]:
-        entries = (
-            FilmWorkDocument.parse_obj(
-                {
-                    field.lower(): value
-                    for field, value in zip(
-                        FilmWorkDocument.schema()["properties"].keys(), row  # type: ignore
-                    )
-                }
-            )
-            for row in rows
-        )
-        return entries
-
-
 class GenrePostgresMerger(PostgresMerger):
     sql = (
         "SELECT g.id, g.name, g.description "
